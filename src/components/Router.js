1f92d94d4a21947a29acc3f723543068e0c167fa
John Callaway <ovation22@users.noreply.github.com>
2020-08-25 18:56:22 -0400

    More routes (#4)

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import Race from '../pages/Race';
+import Train from '../pages/Train';
 import routes from '../constants/routes';
 
 function Router() {
@@ -9,6 +10,7 @@ function Router() {
     <Switch>
       <Route path={routes.root.path} exact component={Home} />
       <Route path={routes.race.path} exact component={Race} />
+      <Route path={routes.train.path} exact component={Train} />
       {/* Render component given un-found route */}
       <Route component={Home} />
     </Switch>
