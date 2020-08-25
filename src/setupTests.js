99e85c71efcfa1289187b09ceda8dacb4eada8f6
John Callaway <john@1north.com>
2020-08-24 22:21:27 -0400

    Routes

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,16 @@
+/* eslint-disable */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import MutationObserver from '@sheerun/mutationobserver-shim';
+window.MutationObserver = MutationObserver;
+
+window.scrollTo = (x) => x
+
+global.matchMedia = media => ({
+    addListener: () => {},
+    removeListener: () => {},
+    matches: media === '(min-width: 1200px)',
+});
